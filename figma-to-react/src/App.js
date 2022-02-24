10a4484baf2b47a1e7b9eb9a5a7e4069ac8898c8
Christy Lo <christylo@Christys-MacBook-Air.local>
2022-02-23 23:28:17 -0800

    added react app and installed styled components

---
+++ figma-to-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Figma to React Workshop Demo :))</h1>
+    </div>
+  );
+}
+
+export default App;
