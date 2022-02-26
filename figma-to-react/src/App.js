cb972ac9c1fb735f934a57c418db7f9399cb7a36
Christy Lo <christylo@Christys-MacBook-Air.local>
2022-02-26 00:11:58 -0800

    created description component

--- figma-to-react/src/App.js
+++ figma-to-react/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Card from './components/Card';
 
 function App() {
+
+  const fillerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
   return (
     <>
       <div className="App">
         <h1>Figma to React Workshop Demo :))</h1>
       </div>
       <div className="Cards">
-        <Card heading="Marley the Retriever"/>
+        <Card heading="Marley the Retriever" description={fillerText}/>
       </div>
     </>
   );
