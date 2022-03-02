c6446d2038958e0c0a67ba3026af90b9825f1ef8
Christy Lo <christylo@dhcp-128-189-71-84.ubcsecure.wireless.ubc.ca>
2022-03-02 12:07:48 -0800

    dryrun-demo

--- figma-to-react/src/App.js
+++ figma-to-react/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
+
+  const fillerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
   return (
-    <div className="App">
-      <h1>Figma to React Workshop Demo :))</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Figma to React Workshop Demo :))</h1>
+      </div>
+      <div className="Cards">
+        <Card heading="Marley the Retriever" description={fillerText}/>
+        <Card heading="Marley the Retriever" description={fillerText}/>
+      </div>
+    </>
   );
 }
 
